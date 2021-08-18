797541814b6a0aded00fb3978832207e3561138f
pokeyjojo <johnny@theschwans.io>
2021-08-18 12:46:24 -0500

    fixing kapi

---
+++ src/love/sprites/week8/kapi.lua
@@ -0,0 +1,147 @@
+	return Sprite (
+    love.graphics.newImage(graphics.imagePath("week8/kapi")),
+	
+	-- Automatically generated from DADDY_DEAREST.xml
+	{
+		{x = 0, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 1: Dad Sing Note DOWN0000
+		{x = 785, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 2: Dad Sing Note DOWN0001
+		{x = 785, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 3: Dad Sing Note DOWN0002
+		{x = 1570, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 4: Dad Sing Note DOWN0003
+		{x = 1570, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 5: Dad Sing Note DOWN0004
+		{x = 2355, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 6: Dad Sing Note DOWN0005
+		{x = 2355, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 7: Dad Sing Note DOWN0006
+		{x = 3140, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 8: Dad Sing Note DOWN0007
+		{x = 3140, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 9: Dad Sing Note DOWN0008
+		{x = 3925, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 10: Dad Sing Note DOWN0009
+		{x = 3925, y = 3364, width = 785, height = 841, offsetX = 250, offsetY = 30, offsetWidth = 555, offsetHeight = 1040}, -- 11: Dad Sing Note DOWN0010
+		{x = 0, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 12: Dad Sing Note LEFT0000
+		{x = 785, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 13: Dad Sing Note LEFT0001
+		{x = 785, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 14: Dad Sing Note LEFT0002
+		{x = 1570, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 15: Dad Sing Note LEFT0003
+		{x = 1570, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 16: Dad Sing Note LEFT0004
+		{x = 2355, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 17: Dad Sing Note LEFT0005
+		{x = 2355, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 18: Dad Sing Note LEFT0006
+		{x = 3140, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 19: Dad Sing Note LEFT0007
+		{x = 3140, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 20: Dad Sing Note LEFT0008
+		{x = 3925, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 21: Dad Sing Note LEFT0009
+		{x = 3925, y = 841, width = 785, height = 841, offsetX = 260, offsetY = -10, offsetWidth = 555, offsetHeight = 1040}, -- 22: Dad Sing Note LEFT0010
+		{x = 0, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 23: Dad Sing Note RIGHT0000
+		{x = 785, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 24: Dad Sing Note RIGHT0001
+		{x = 785, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 25: Dad Sing Note RIGHT0002
+		{x = 1570, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 26: Dad Sing Note RIGHT0003
+		{x = 1570, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 27: Dad Sing Note RIGHT0004
+		{x = 2355, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 28: Dad Sing Note RIGHT0005
+		{x = 2355, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 29: Dad Sing Note RIGHT0006
+		{x = 3140, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 30: Dad Sing Note RIGHT0007
+		{x = 3140, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 31: Dad Sing Note RIGHT0008
+		{x = 3925, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 32: Dad Sing Note RIGHT0009
+		{x = 3925, y = 2523, width = 785, height = 841, offsetX = 250, offsetY = -25, offsetWidth = 555, offsetHeight = 1040}, -- 33: Dad Sing Note RIGHT0010
+		{x = 0, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 34: Dad Sing Note UP0000
+		{x = 785, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 35: Dad Sing Note UP0001
+		{x = 785, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 36: Dad Sing Note UP0002
+		{x = 1570, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 37: Dad Sing Note UP0003
+		{x = 1570, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 38: Dad Sing Note UP0004
+		{x = 2355, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 39: Dad Sing Note UP0005
+		{x = 2355, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 40: Dad Sing Note UP0006
+		{x = 3140, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 41: Dad Sing Note UP0007
+		{x = 3140, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 42: Dad Sing Note UP0008
+		{x = 3925, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 43: Dad Sing Note UP0009
+		{x = 3925, y = 1682, width = 785, height = 841, offsetX = 256, offsetY = -50, offsetWidth = 555, offsetHeight = 1040}, -- 44: Dad Sing Note UP0010
+		{x = 0, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 45: Dad idle dance0000
+		{x = 0, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 46: Dad idle dance0001
+		{x = 785, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 47: Dad idle dance0002
+		{x = 785, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 48: Dad idle dance0003
+		{x = 1570, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 49: Dad idle dance0004
+		{x = 1570, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 50: Dad idle dance0005
+		{x = 2355, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 51: Dad idle dance0006
+		{x = 2355, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 52: Dad idle dance0007
+		{x = 3140, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 53: Dad idle dance0008
+		{x = 3140, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 54: Dad idle dance0009
+		{x = 3925, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 55: Dad idle dance0010
+		{x = 3925, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 56: Dad idle dance0011
+		{x = 0, y = 0, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 57: Dad idle dance0012
+		{x = 0, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 58: Dad meow0000
+		{x = 785, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 59: Dad meow0001
+		{x = 785, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 60: Dad meow0002
+		{x = 1570, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 61: Dad meow0003
+		{x = 1570, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 62: Dad meow0004
+		{x = 2355, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 63: Dad meow0005
+		{x = 2355, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 64: Dad meow0006
+		{x = 3140, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 65: Dad meow0007
+		{x = 3140, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 66: Dad meow0008
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 67: Dad meow0009
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 68: Dad meow0010
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 69: Dad meow0011
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 70: Dad meow0012
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 71: Dad meow0013
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 72: Dad meow0014
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 73: Dad meow0015
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 74: Dad meow0016
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 75: Dad meow0017
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 76: Dad meow0018
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 77: Dad meow0019
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 78: Dad meow0020
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 79: Dad meow0021
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 80: Dad meow0022
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 81: Dad meow0023
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 82: Dad meow0024
+		{x = 3925, y = 4205, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 83: Dad meow0025
+		{x = 0, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 84: Dad stare0001
+		{x = 0, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 85: Dad stare0002
+		{x = 785, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 86: Dad stare0003
+		{x = 785, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 87: Dad stare0004
+		{x = 785, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 88: Dad stare0005
+		{x = 1570, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 89: Dad stare0006
+		{x = 1570, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 90: Dad stare0007
+		{x = 2355, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 91: Dad stare0008
+		{x = 2355, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 92: Dad stare0009
+		{x = 3140, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 93: Dad stare0010
+		{x = 3140, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 94: Dad stare0011
+		{x = 3925, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 95: Dad stare0012
+		{x = 3925, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 96: Dad stare0013
+		{x = 3925, y = 5046, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 97: Dad stare0014
+		{x = 0, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 98: Dad stare0015
+		{x = 0, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 99: Dad stare0016
+		{x = 785, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 100: Dad stare0017
+		{x = 785, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 101: Dad stare0018
+		{x = 1570, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 102: Dad stare0019
+		{x = 1570, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 103: Dad stare0020
+		{x = 2355, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 104: Dad stare0021
+		{x = 2355, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 105: Dad stare0022
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 106: Dad stare0023
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 107: Dad stare0024
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 108: Dad stare0025
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 109: Dad stare0026
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 110: Dad stare0027
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 111: Dad stare0028
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 112: Dad stare0029
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 113: Dad stare0030
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 114: Dad stare0031
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 115: Dad stare0032
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 116: Dad stare0033
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 117: Dad stare0034
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 118: Dad stare0035
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 119: Dad stare0036
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 120: Dad stare0037
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 121: Dad stare0038
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 122: Dad stare0039
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 123: Dad stare0040
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 124: Dad stare0041
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 125: Dad stare0042
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 126: Dad stare0043
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 127: Dad stare0044
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 128: Dad stare0045
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 129: Dad stare0046
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040}, -- 130: Dad stare0047
+		{x = 3140, y = 5887, width = 785, height = 841, offsetX = 250, offsetY = 0, offsetWidth = 555, offsetHeight = 1040} -- 131: Dad stare0048
+	},
+{
+["down"] = {start = 1, stop = 11, speed = 24, offsetX = 0, offsetY = 0},
+        ["left"] = {start = 12, stop = 22, speed = 24, offsetX = 18, offsetY = 5},
+        ["right"] = {start = 23, stop = 33, speed = 24, offsetX = 0, offsetY = 0},
+        ["up"] = {start = 34, stop = 44, speed = 24, offsetX = 0, offsetY = 0},
+        ["idle"] = {start = 45, stop = 57, speed = 24, offsetX = 0, offsetY = 0}
+    },
+    "idle",
+    false
+)
